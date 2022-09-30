69b9a03d418205775c85b60ce47c64b3d1a22a0d
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-09-30 17:28:40 +0530

    Day2_Of_100DaysOfCodingChallenge
    Program Of BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+package com.TrX;
+
+//Program of BubbleSort     #Day2Of100DaysOfCoding
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int [] arr1 = {67,54,89,11,56};
+        System.out.println("Before Sorting");
+        System.out.println(Arrays.toString(arr1));
+        bubbleSort(arr1);
+        System.out.println("After Sorting");
+        System.out.println(Arrays.toString(arr1));
+    }
+    public static void bubbleSort(int [] arr){
+        int temp = 0;
+        for(int i=0; i < arr.length; i++){
+            for(int j=1; j < (arr.length-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
