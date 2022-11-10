88f5e7066128329b560628f49d61241e05376243
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-11-10 23:03:32 +0530

    Day22_Of_100DaysOfCodingChallenge

---
+++ Day_22_AverageTempCalculator.java
@@ -0,0 +1,30 @@
+package com.TrX;
+
+import java.util.Scanner;
+
+public class Day_22_AverageTempCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of Days :");
+        int numDays = sc.nextInt();
+        int [] temp = new int [numDays];
+        int sum=0;
+        for(int i =0 ; i<temp.length;i++){
+            System.out.println("Day "+(i+1)+"'s Temperature");
+            temp[i]= sc.nextInt();
+            sum += temp[i];
+        }
+        int Average = sum/numDays;
+        System.out.println("Average temperature is :"+Average);
+        int above=0;
+        for(int i=0; i<temp.length;i++){
+            if(temp[i]>Average){
+                above++;
+            }
+        }
+        System.out.println("Days having temperature above average :"+above);
+    }
+}
---
+++ README.txt
@@ -0,0 +1,23 @@
+Q)Average temprature Calculator in java'
+
+
+
+
+Input-
+Enter the number of Days :
+4
+Day 1's Temperature
+10
+Day 2's Temperature
+20
+Day 3's Temperature
+14
+Day 4's Temperature
+8
+
+Output-
+Average temperature is :13
+Days having temperature above average :2
+
+
+
