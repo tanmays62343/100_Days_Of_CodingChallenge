953d05d36e0510646860837c498d3691b7c4bb14
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-11-11 23:43:20 +0530

    Add files via upload

---
+++ callByRefrence.java
@@ -0,0 +1,17 @@
+package com.TrX;
+
+public class callByRefrence {
+
+    int a ;
+    int b ;
+
+    public callByRefrence(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+
+
+
+
+}
