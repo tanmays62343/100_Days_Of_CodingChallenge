feec331b85d4a7e65c052d5320f5f2fdccf0d445
Tanmay Deopurkar <114358603+tanmays62343@users.noreply.github.com>
2022-11-13 22:08:02 +0530

    Day25_Of_100DaysofCodingChallenge

---
+++ Day_25_Palindrome_HackerRank.java
@@ -0,0 +1,24 @@
+package com.TrX;
+import java.util.Scanner;
+public class Day_25_Palindrome_HackerRank {
+        public static char ch;
+        public static String Rev="";
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String S = sc.nextLine();
+            System.out.println(palindrome(S));
+        }
+        public static String palindrome(String S){
+
+            for(int i=0;i<S.length();i++){
+                ch=S.charAt(i);
+                Rev=ch+Rev;
+            }
+            if(S.equals(Rev)){
+                return "Yes";
+            }
+            return "No";
+        }
+}
+
