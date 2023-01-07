d52629c2d51df838b1afdd8b579175880c2d7483
Tanmay <vs62343@gmail.com>
2023-01-07 14:30:18 +0530

    Day51

---
+++ Day_51_PatternSyntaxChecker_HackerRank.java
@@ -0,0 +1,23 @@
+package com.TrX;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class Day_51_PatternSyntaxChecker_HackerRank {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        scan.nextLine();
+        for(int i = num; i > 0; i--){
+            String pat = scan.nextLine();
+            try{
+                Pattern reg = Pattern.compile(pat);
+                System.out.println("Valid");
+            }
+            catch(Exception e){
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
